7afc649acfdf32e4c755463613084c7075ddce16
LARS1\Lars Kirchhofer <lars@kirchhofer.de>
2019-03-18 16:41:01 +0100

    ItemActivate eingebaut

--- Personen_db_wpf/Personen_db_wpf/MainWindow.xaml.cs
+++ Personen_db_wpf/Personen_db_wpf/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace Personen_db_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Person myPerson = new Person();
+        public AddressDB myDB = new AddressDB();
+        public Person SelectedPerson { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            ShowAddresses();
+        }
+
+        private void ShowAddresses()
+        {
+            List<Person> myAddresses = new List<Person>();
+            myAddresses = myDB.ReadDB();
+            foreach (Person entry in myAddresses)
+            {
+                ListViewItem lvi = new ListViewItem();
+                listView1.Items.Add(entry);
+            }
+        }
+
+        private void ItemActivate(object sender, MouseButtonEventArgs e)
+        {
+            ListViewEntry_Selected();
+        }
+
+        private void ListViewEntry_Selected()
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                //ListViewItem lvi = listView1.SelectedItems[0];
+                myPerson = (Person)listView1.Items.GetItemAt(listView1.SelectedIndex);
+                TextBoxFname.Text = myPerson.FName;
+                TextBoxLname.Text = myPerson.LName;
+                TextBoxStreet.Text = myPerson.Street;
+                TextBoxNumber.Text = myPerson.Number;
+                TextBoxPlz.Text = myPerson.Plz;
+                TextBoxLocation.Text = myPerson.Location;
+                TextBoxTelephone.Text = myPerson.Telephone;
+                TextBoxEmail.Text = myPerson.Email;
+            }
         }
     }
 }
