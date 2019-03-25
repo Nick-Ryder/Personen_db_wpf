5fb1e9df19ebbae4269ea18182e0c7a4e4265af7
LARS1\Lars Kirchhofer <lars@kirchhofer.de>
2019-03-25 23:43:04 +0100

    MainWindow.xaml.cs

--- Personen_db_wpf/Personen_db_wpf/MainWindow.xaml.cs
+++ Personen_db_wpf/Personen_db_wpf/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace Personen_db_wpf
         {
             InitializeComponent();
             ShowAddresses();
+            LockForm();
         }
 
         private void ShowAddresses()
@@ -36,14 +37,70 @@ namespace Personen_db_wpf
             myAddresses = myDB.ReadDB();
             foreach (Person entry in myAddresses)
             {
-                ListViewItem lvi = new ListViewItem();
                 listView1.Items.Add(entry);
             }
         }
 
+        public void Insert_Click(object sender, EventArgs e)
+        {
+            myPerson.FName = TextBoxFname.Text;
+            myPerson.LName = TextBoxLname.Text;
+            myPerson.Street = TextBoxStreet.Text;
+            myPerson.Number = TextBoxNumber.Text;
+            myPerson.Plz = TextBoxPlz.Text;
+            myPerson.Location = TextBoxLocation.Text;
+            myPerson.Telephone = TextBoxTelephone.Text;
+            myPerson.Email = TextBoxEmail.Text;
+            // Personendaten in mdf eintragen
+            int index = myDB.InsertDB(myPerson);            // index = Primärschlüssel des geänderten Eintrags in DB
+            myPerson.Id = index;
+            
+            // Daten in Listview eintragen
+            listView1.Items.Add(myPerson);
+            ClearForm();
+            LockForm();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                myPerson = (Person)listView1.Items.GetItemAt(listView1.SelectedIndex);
+                //MessageBox.Show(listView1.SelectedIndices[0].ToString(), "Hallo", MessageBoxButtons.OK);
+                MessageBox.Show(myPerson.Id.ToString(), "Hallo", MessageBoxButton.OK);
+                if (myDB.RemoveFromDb(myPerson.Id))
+                {
+                    listView1.Items.RemoveAt(listView1.SelectedIndex);
+                    ClearForm();
+                    LockForm();
+                    Insert.IsEnabled = false;
+                    Delete.IsEnabled = false;
+                    Change.IsEnabled = false;
+                    New.IsEnabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Datenbank Fehler", "Achtung", MessageBoxButton.OK);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Kein Eintrag selektiert!", "Achtung", MessageBoxButton.OK);
+            }
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            ClearForm();
+            UnlockForm();
+            Delete.IsEnabled = false;
+            Change.IsEnabled = false;
+            TextBoxFname.Focus();
+        }
+
         private void ItemActivate(object sender, MouseButtonEventArgs e)
         {
-            ListViewEntry_Selected();
+            ListViewEntry_Selected(); 
         }
 
         private void ListViewEntry_Selected()
@@ -60,7 +117,56 @@ namespace Personen_db_wpf
                 TextBoxLocation.Text = myPerson.Location;
                 TextBoxTelephone.Text = myPerson.Telephone;
                 TextBoxEmail.Text = myPerson.Email;
+
+                UnlockForm();
+                Insert.IsEnabled = true;
+                Change.IsEnabled = true;
+                Delete.IsEnabled = true;
             }
+        }
+
+        private void ClearForm()
+        {
+            TextBoxFname.Text = "";
+            TextBoxLname.Text = "";
+            TextBoxStreet.Text = "";
+            TextBoxNumber.Text = "";
+            TextBoxPlz.Text = "";
+            TextBoxLocation.Text = "";
+            TextBoxTelephone.Text = "";
+            TextBoxEmail.Text = "";
+        }
+
+        private void LockForm()
+        {
+            TextBoxFname.IsEnabled = false;
+            TextBoxLname.IsEnabled = false;
+            TextBoxStreet.IsEnabled = false;
+            TextBoxNumber.IsEnabled = false;
+            TextBoxPlz.IsEnabled = false;
+            TextBoxLocation.IsEnabled = false;
+            TextBoxTelephone.IsEnabled = false;
+            TextBoxEmail.IsEnabled = false;
+            Insert.IsEnabled = false;
+            Change.IsEnabled = false;
+            Delete.IsEnabled = false;
+            New.IsEnabled = true;
+        }
+
+        private void UnlockForm()
+        {
+            TextBoxFname.IsEnabled = true;
+            TextBoxLname.IsEnabled = true;
+            TextBoxStreet.IsEnabled = true;
+            TextBoxNumber.IsEnabled = true;
+            TextBoxPlz.IsEnabled = true;
+            TextBoxLocation.IsEnabled = true;
+            TextBoxTelephone.IsEnabled = true;
+            TextBoxEmail.IsEnabled = true;
+            Insert.IsEnabled = true;
+            Change.IsEnabled = true;
+            Delete.IsEnabled = true;
+            New.IsEnabled = true;
         }
     }
 }
